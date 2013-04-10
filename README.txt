5ebd538f73450b6a9ea5ff218f2200badaf1f759
Phil Adams <phil@philadams.net>
2013-04-10 16:46:11 -0400

    proof of concept load local image and display in canvas

--- README.txt
+++ README.txt
@@ -6,4 +6,4 @@ Generate door decorations for university resident halls.
 about
 -----
 
-breadnut is an excuse to play with Javascript APIs and Angular.js.
+breadnut is an excuse to play with several Javascript APIs and Angular.js.
--- js/breadnut.js
+++ js/breadnut.js
@@ -1,7 +1,26 @@
-var handleResidentData = function(event) {
+var loadImage = function(event) {
     var file = event.target.files[0];
     var reader = new FileReader();
-    reader.readAsText(file);
+    var image = new Image();
+    var canvas = document.getElementById('canvas');
+    var ctx = canvas.getContext('2d');
+
+    // handle file onload
+    reader.onload = function(fh) {
+        return function(e) {
+            var fileContents = e.target.result;
+            image.src = fileContents;
+            canvas.setAttribute('width', image.width);
+            canvas.setAttribute('height', image.height);
+            ctx.drawImage(image, 20, 20);
+        };
+    }(file);
+    reader.readAsDataURL(file);
+};
+
+var loadResidentData = function(event) {
+    var file = event.target.files[0];
+    var reader = new FileReader();
 
     // handle file onload
     reader.onload = function(fh) {
@@ -12,6 +31,9 @@ var handleResidentData = function(event) {
             console.log(data);
         };
     }(file);
+    reader.readAsText(file);
 };
 
-document.getElementById('residents-file').addEventListener('change', handleResidentData, false);
+// adding event handlers
+document.getElementById('residents-file').addEventListener('change', loadResidentData, false);
+document.getElementById('image-file').addEventListener('change', loadImage, false);
