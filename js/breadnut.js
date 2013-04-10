5f083891deb470b109aca6fbc10ae2ca2a56efaa
Phil Adams <phil@philadams.net>
2013-04-10 16:14:26 -0400

    proof of concept FileReader and d3.csv.parse

--- js/breadnut.js
+++ js/breadnut.js
@@ -1,1 +1,17 @@
-console.log('hello world');
+var handleResidentData = function(event) {
+    var file = event.target.files[0];
+    var reader = new FileReader();
+    reader.readAsText(file);
+
+    // handle file onload
+    reader.onload = function(fh) {
+        return function(e) {
+            var fileContents = e.target.result;
+            console.log(fileContents);
+            var data = d3.csv.parse(fileContents);
+            console.log(data);
+        };
+    }(file);
+};
+
+document.getElementById('residents-file').addEventListener('change', handleResidentData, false);
